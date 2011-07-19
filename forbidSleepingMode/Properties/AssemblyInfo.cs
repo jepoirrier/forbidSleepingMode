0e098cb7a964fd6fc62d85173aa4fceaf6f5e748
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2011-07-19 23:27:34 +0200

    Initial version

---
+++ forbidSleepingMode/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("forbid Sleeping Mode")]
+[assembly: AssemblyDescription("Forbids your computer to enter sleep by tickling it every 10 minutes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jep")]
+[assembly: AssemblyProduct("forbidSleepingMode")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c29ad9e4-94ec-49ad-bed9-e2d9fcc0d3d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
